ab6dc9ca1df3cae60eda05e6f4d6189a9f4d8f49
m.ryndin <m.ryndin@dvue.ru>
2026-05-27 09:28:32 +0300

    Add 1.1

---
+++ frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { ReactKeycloakProvider } from '@react-keycloak/web';
+import Keycloak, { KeycloakConfig } from 'keycloak-js';
+import ReportPage from './components/ReportPage';
+
+const keycloakConfig: KeycloakConfig = {
+  url: process.env.REACT_APP_KEYCLOAK_URL,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM || "",
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID || ""
+};
+
+const keycloak = new Keycloak(keycloakConfig);
+
+// Опции инициализации для активации PKCE
+const keycloakInitOptions = {
+  pkceMethod: 'S256',
+  onLoad: 'check-sso',
+};
+
+const App: React.FC = () => {
+  return (
+    <ReactKeycloakProvider 
+      authClient={keycloak} 
+      initOptions={keycloakInitOptions}
+    >
+      <div className="App">
+        <ReportPage />
+      </div>
+    </ReactKeycloakProvider>
+  );
+};
+
+export default App;
